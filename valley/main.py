0dce450f364b1615579b90a8a0492c6a2e2bf8fc
laboodabedibdab <104901194+laboodabedibdab@users.noreply.github.com>
2022-05-19 00:13:29 +0300

    Update main.py

--- valley/main.py
+++ valley/main.py
@@ -1,3 +1,11 @@
 import eel
+
 eel.init("web")
-eel.start("main.html", size=(700, 400))
+
+
+@eel.expose
+def get_inf(nick, password, tel):
+    print(nick, password, tel)
+
+
+eel.start("main.html", mode="chrome", size=(700, 400))
